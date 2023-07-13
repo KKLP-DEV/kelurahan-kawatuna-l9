463f433327cdd23471231fd1ab4a9618e4e58663
rizki <muhammadrizkitkj123@gmail.com>
2023-07-13 14:56:27 +0800

    update model surat masuk

--- app/Models/SuratMasukModel.php
+++ app/Models/SuratMasukModel.php
@@ -10,7 +10,7 @@ class SuratMasukModel extends Model
     use HasFactory;
     protected $table = 'tb_surat_masuk';
     protected $fillable = [
-        'id', 'uuid', 'nomor_surat', 'tanggal_surat', 'id_tahun', 'id_jenis_surat', 'file_surat', 'asal_surat', 'created_at', 'updated_at'
+        'id', 'uuid','id_user', 'nomor_surat', 'tanggal_surat', 'id_tahun', 'id_jenis_surat', 'file_surat', 'asal_surat', 'created_at', 'updated_at'
     ];
 
     public function tahun()
@@ -21,6 +21,11 @@ class SuratMasukModel extends Model
     public function jenis_surat()
     {
         return $this->belongsTo(JenisSuratModel::class, 'id_jenis_surat');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'id_user');
     }
 
     public function getTahun($id_tahun)
@@ -38,6 +43,15 @@ class SuratMasukModel extends Model
         $data = $this->join('tb_jenis_surat', 'tb_surat_masuk.id_jenis_surat', '=', 'tb_jenis_surat.id')
             ->select('tb_jenis_surat.uuid', 'tb_jenis_surat.jenis_surat')
             ->where('tb_surat_masuk.id_jenis_surat', '=', $id_jenis_surat)
+            ->first();
+        return $data;
+    }
+
+    public function getUser($id_user)
+    {
+        $data = $this->join('users', 'tb_surat_masuk.id_user', '=', 'users.id')
+            ->select('users.uuid', 'users.name', 'users.email')
+            ->where('tb_surat_masuk.id_user', '=', $id_user)
             ->first();
         return $data;
     }
