f0c4d931e3df56e2a9c670653ed0b2665bc740d9
rizki <muhammadrizkitkj123@gmail.com>
2023-07-13 14:59:23 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\JenisSuratController;
 use App\Http\Controllers\API\SuratMasukController;
 use App\Http\Controllers\API\TahunController;
@@ -18,55 +19,72 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('backend.dashboard');
-});
 
-Route::get('/cms/tahun', function () {
-    return view('backend.tahun');
-});
-Route::get('/cms/surat/masuk', function () {
-    return view('backend.surat-masuk');
-});
-Route::get('/cms/jenis/surat', function () {
-    return view('backend.jenis-surat');
-});
-
-Route::get('/cms/arsip/{uuid}', function () {
-    return view('backend.arsip');
-});
-Route::get('/cms/arsip/{id_tahun}/{id_jenis_surat}', function () {
-    return view('backend.arsip-surat-masuk');
+//auth
+Route::prefix('v4')->controller(AuthController::class)->group(function () {
+    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/auth/verify-email/{email}', 'verifyEmail');
+    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/auth/register', 'register');
+    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/auth/login', 'login');
 });
 
 
 
-Route::prefix('v1')->controller(TahunController::class)->group(function () {
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun', 'getAllData');
-    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/create', 'createData');
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/get/{uuid}', 'getDataByUuid');
-    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/update/{uuid}', 'updateDataByUuid');
-    Route::delete('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/delete/{uuid}', 'deleteData');
+Route::middleware('web', 'auth')->group(function () {
+    Route::get('/', function () {
+        return view('backend.dashboard');
+    });
+
+    Route::get('/cms/tahun', function () {
+        return view('backend.tahun');
+    });
+    Route::get('/cms/surat/masuk', function () {
+        return view('backend.surat-masuk');
+    });
+    Route::get('/cms/jenis/surat', function () {
+        return view('backend.jenis-surat');
+    });
+
+    Route::get('/cms/arsip/{uuid}', function () {
+        return view('backend.arsip');
+    });
+    Route::get('/cms/arsip/{id_tahun}/{id_jenis_surat}', function () {
+        return view('backend.arsip-surat-masuk');
+    });
+
+
+
+    //tahun arsip
+    Route::prefix('v1')->controller(TahunController::class)->group(function () {
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun', 'getAllData');
+        Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/create', 'createData');
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/get/{uuid}', 'getDataByUuid');
+        Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/update/{uuid}', 'updateDataByUuid');
+        Route::delete('/396d6585-16ae-4d04-9549-c499e52b75ea/tahun/delete/{uuid}', 'deleteData');
+    });
+
+    //jenis surat
+    Route::prefix('v2')->controller(JenisSuratController::class)->group(function () {
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat', 'getAllData');
+        Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/create', 'createData');
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/get/{uuid}', 'getDataByUuid');
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/get/id/{id}', 'getDataById');
+        Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/update/{uuid}', 'updateDataByUuid');
+        Route::delete('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/delete/{uuid}', 'deleteData');
+    });
+
+    //surat masuk 
+    Route::prefix('v3')->controller(SuratMasukController::class)->group(function () {
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk', 'getAllData');
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/user/{id_tahun}/{id_jenis_surat}', 'getDataByUser');
+        // Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/get/{id_tahun}/{id_jenis_surat}', 'getDataByTahunAndJenisSurat');
+        Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/create', 'createData');
+        Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/get/{uuid}', 'getDataByUuid');
+        Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/update/{uuid}', 'updateDataByUuid');
+        Route::delete('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/delete/{uuid}', 'deleteData');
+    });
 });
 
-Route::prefix('v2')->controller(JenisSuratController::class)->group(function () {
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat', 'getAllData');
-    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/create', 'createData');
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/get/{uuid}', 'getDataByUuid');
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/get/id/{id}', 'getDataById');
-    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/update/{uuid}', 'updateDataByUuid');
-    Route::delete('/396d6585-16ae-4d04-9549-c499e52b75ea/jenis/surat/delete/{uuid}', 'deleteData');
+
+Route::get('/login', function () {
+    return view('auth.login');
 });
-
-Route::prefix('v3')->controller(SuratMasukController::class)->group(function () {
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk', 'getAllData');
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/get/{id_tahun}/{id_jenis_surat}', 'getDataByTahunAndJenisSurat');
-    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/create', 'createData');
-    Route::get('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/get/{uuid}', 'getDataByUuid');
-    Route::post('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/update/{uuid}', 'updateDataByUuid');
-    Route::delete('/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/delete/{uuid}', 'deleteData');
-});
-
-
-
-
