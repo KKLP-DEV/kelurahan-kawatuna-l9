09893c66725f8292b3cd60cb1bb8d216299687d1
rizki <muhammadrizkitkj123@gmail.com>
2023-07-22 14:24:47 +0800

    update

--- resources/views/Layouts/Sidebar.blade.php
+++ resources/views/Layouts/Sidebar.blade.php
@@ -2,7 +2,7 @@
  <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
          <div class="sidebar-brand-icon">
-             <img src="{{ asset('img/logo1.png') }}" >
+             <img src="{{ asset('img/logo1.png') }}">
          </div>
          <div class="sidebar-brand-text">Kelurahan Kawatuna</div>
      </a>
@@ -19,21 +19,21 @@
          Data
      </div>
      @if (auth()->user()->role == 1)
-     <li class="nav-item  {{ request()->is('cms/tahun') ? 'active' : '' }}">
-         <a class="nav-link " href="{{ url('cms/tahun') }}" data-target="#collapsePage" aria-expanded="true"
-             aria-controls="collapsePage">
-             <i class="fa-solid fa-calendar-days "></i>
-             <span>Tahun Arsip</span>
-         </a>
-     </li>
-     <li class="nav-item  {{ request()->is('cms/jenis/surat') ? 'active' : '' }}">
-        <a class="nav-link " href="{{ url('cms/jenis/surat') }}" data-target="#collapsePage" aria-expanded="true"
-            aria-controls="collapsePage">
-            <i class="fa-sharp fa-solid fa-filter"></i>
-            <span>Jenis Surat</span>
-        </a>
-    </li>
-    @endif
+         <li class="nav-item  {{ request()->is('cms/tahun') ? 'active' : '' }}">
+             <a class="nav-link " href="{{ url('cms/tahun') }}" data-target="#collapsePage" aria-expanded="true"
+                 aria-controls="collapsePage">
+                 <i class="fa-solid fa-calendar-days "></i>
+                 <span>Tahun Arsip</span>
+             </a>
+         </li>
+         <li class="nav-item  {{ request()->is('cms/jenis/surat') ? 'active' : '' }}">
+             <a class="nav-link " href="{{ url('cms/jenis/surat') }}" data-target="#collapsePage" aria-expanded="true"
+                 aria-controls="collapsePage">
+                 <i class="fa-sharp fa-solid fa-filter"></i>
+                 <span>Jenis Surat</span>
+             </a>
+         </li>
+     @endif
      <li class="nav-item  {{ request()->is('cms/surat/masuk') ? 'active' : '' }}">
          <a class="nav-link " href="{{ url('cms/surat/masuk') }}" data-target="#collapsePage" aria-expanded="true"
              aria-controls="collapsePage">
@@ -42,51 +42,51 @@
          </a>
      </li>
      <li class="nav-item  {{ request()->is('cms/surat/keluar') ? 'active' : '' }}">
-        <a class="nav-link " href="{{ url('cms/surat/keluar') }}" data-target="#collapsePage" aria-expanded="true"
-            aria-controls="collapsePage">
-            <i class="fa-solid fa-upload"></i>
-            <span>Surat Keluar</span>
-        </a>
-    </li>
+         <a class="nav-link " href="{{ url('cms/surat/keluar') }}" data-target="#collapsePage" aria-expanded="true"
+             aria-controls="collapsePage">
+             <i class="fa-solid fa-upload"></i>
+             <span>Surat Keluar</span>
+         </a>
+     </li>
      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuratMasuk"
-            aria-expanded="false" aria-controls="collapseSuratMasuk">
-            <i class="fa-solid fa-box-archive"></i>
-            <span>Data Arsip Surat Masuk</span>
-        </a>
-        <div id="collapseSuratMasuk" class="collapse" aria-labelledby="headingSuratMasuk"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Arsip Tahun</h6>
-                <div id="dropdownContent"></div> 
-            </div>
-        </div>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuratKeluar"
-            aria-expanded="false" aria-controls="collapseSuratKeluar">
-            <i class="fa-solid fa-box-archive"></i>
-            <span>Data Arsip Surat Keluar</span>
-        </a>
-        <div id="collapseSuratKeluar" class="collapse" aria-labelledby="headingSuratKeluar"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Arsip Tahun</h6>
-                <div id="dropdownContents"></div> 
-            </div>
-        </div>
-    </li>
-    
-  
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuratMasuk"
+             aria-expanded="false" aria-controls="collapseSuratMasuk">
+             <i class="fa-solid fa-box-archive"></i>
+             <span>Data Arsip Surat Masuk</span>
+         </a>
+         <div id="collapseSuratMasuk" class="collapse" aria-labelledby="headingSuratMasuk"
+             data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">Arsip Tahun</h6>
+                 <div id="dropdownContent"></div>
+             </div>
+         </div>
+     </li>
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuratKeluar"
+             aria-expanded="false" aria-controls="collapseSuratKeluar">
+             <i class="fa-solid fa-box-archive"></i>
+             <span>Data Arsip Surat Keluar</span>
+         </a>
+         <div id="collapseSuratKeluar" class="collapse" aria-labelledby="headingSuratKeluar"
+             data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">Arsip Tahun</h6>
+                 <div id="dropdownContents"></div>
+             </div>
+         </div>
+     </li>
+
+
      <hr class="sidebar-divider">
-        <!-- Logout -->
-        <li class="nav-item">
-            <a class="nav-link" href="javascript:void(0);" id="logoutButton">
-                <i class="fas fa-sign-out-alt fa-fw"></i>
-                <span>Logout</span>
-            </a>
-        </li>
-        {{-- @endif --}}
+     <!-- Logout -->
+     <li class="nav-item">
+         <a class="nav-link" href="javascript:void(0);" id="logoutButton">
+             <i class="fas fa-sign-out-alt fa-fw"></i>
+             <span>Logout</span>
+         </a>
+     </li>
+     {{-- @endif --}}
 
  </ul>
 
@@ -115,7 +115,7 @@
          });
      });
 
-     function fetchYears() {
+     function fetchYears(urlSegment, targetDropdownId) {
          $.ajax({
              url: "{{ url('v1/396d6585-16ae-4d04-9549-c499e52b75ea/tahun') }}",
              method: "GET",
@@ -124,26 +124,29 @@
                  console.log(response);
                  var dropdownContent = "";
                  response.data.forEach(function(data) {
-                     dropdownContent += '<a class="collapse-item" href="{{ url('cms/arsip') }}/' +
-                         data.id +
+                     dropdownContent +=
+                         '<a class="collapse-item" href="{{ url('cms/arsip/surat') }}/' +
+                         urlSegment +
+                         '/get/' + data.id +
                          '">' +
                          data.tahun +
                          '</a>';
                  });
-                 $('#dropdownContent').html(dropdownContent);
-                 $('#dropdownContents').html(dropdownContent);
+                 $('#' + targetDropdownId).html(dropdownContent);
              },
              error: function(xhr, status, error) {
                  console.log(xhr.responseText);
-                 alert('Error: Failed to fetch years. Please try again.');
+                 alert('Error: Gagal mengambil data tahun. Silakan coba lagi.');
              }
          });
      }
 
      $(document).ready(function() {
-         fetchYears();
+         fetchYears('masuk', 'dropdownContent');
+         fetchYears('keluar', 'dropdownContents');
          $('#collapsePage').on('shown.bs.collapse', function() {
-             fetchYears();
+             fetchYears('masuk', 'dropdownContent');
+             fetchYears('keluar', 'dropdownContents');
          });
      });
  </script>
