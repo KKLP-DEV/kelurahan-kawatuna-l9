144e613b754f2c3feccb3890ac942db9fcc26d1e
rizki <muhammadrizkitkj123@gmail.com>
2023-07-27 10:09:50 +0800

    update new field in db

--- app/Models/SuratKeluarModel.php
+++ app/Models/SuratKeluarModel.php
@@ -10,7 +10,7 @@ class SuratKeluarModel extends Model
     use HasFactory;
     protected $table = 'tb_surat_keluar';
     protected $fillable = [
-        'id', 'uuid','id_user', 'nomor_surat', 'tanggal_surat', 'id_tahun', 'id_jenis_surat', 'file_surat', 'tujuan_surat', 'created_at', 'updated_at'
+        'id', 'uuid','id_user', 'nomor_surat', 'tanggal_surat', 'id_tahun', 'id_jenis_surat', 'file_surat', 'tujuan_surat','perihal', 'created_at', 'updated_at'
     ];
 
     public function tahun()
