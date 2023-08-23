000d303d8c02b58f358d4176f158a6354ed409cf
rizki <muhammadrizkitkj123@gmail.com>
2023-08-23 12:53:11 +0800

    udpate

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
-        'v4/396d6585-16ae-4d04-9549-c499e52b75ea/auth/register',
-        'v3/396d6585-16ae-4d04-9549-c499e52b75ea/surat-masuk/create',
-        'v4/396d6585-16ae-4d04-9549-c499e52b75ea/auth/login'
+     
     ];
 }
