4498fb23f1ec1527a02a3ab6e4b363f9326b7996
rizki <muhammadrizkitkj123@gmail.com>
2023-08-23 12:52:21 +0800

    update kerner

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,8 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            
         ],
 
         'api' => [
@@ -66,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'adminlevel' => \App\Http\Middleware\AdminLevel::class,
+        'after' => \App\Http\Middleware\RedirectAfterLogin::class,
     ];
 }
