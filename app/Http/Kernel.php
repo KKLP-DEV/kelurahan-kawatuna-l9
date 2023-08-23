a2f8b41340f0ff9e792491554c782bbc7aa82a1b
rizki <muhammadrizkitkj123@gmail.com>
2023-08-23 12:54:37 +0800

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'adminlevel' => \App\Http\Middleware\AdminLevel::class,
-        'after' => \App\Http\Middleware\RedirectAfterLogin::class,
     ];
 }
